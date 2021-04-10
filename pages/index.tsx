31f801c91998174b49fdf5669ee228135a93d126
wzulfikar <me@wzulfikar.com>
2021-04-10 13:28:53 +0700

    chore: remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
+import Head from 'next/head'
 import Container from '../components/container'
 import MoreStories from '../components/more-stories'
 import HeroPost from '../components/hero-post'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
 import Post from '../types/post'
 
 type Props = {
@@ -15,6 +14,7 @@ type Props = {
 const Index = ({ allPosts }: Props) => {
   const heroPost = allPosts[0]
   const morePosts = allPosts.slice(1)
+
   return (
     <>
       <Layout>
