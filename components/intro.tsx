4e686ef05327647a159c08ba237896772b49e97b
wzulfikar <me@wzulfikar.com>
2021-04-10 13:33:18 +0700

    chore: remove unnecessary code

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,10 +1,8 @@
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 import { useGreeting } from '@lib/valtio'
 import { GITHUB_REPO } from '@lib/constants'
 
 const Intro = () => {
-  const greetingRef = useRef(null)
-
   const { greeting, shuffle } = useGreeting()
 
   useEffect(() => {
@@ -19,7 +17,7 @@ const Intro = () => {
 
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 ref={greetingRef} className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8 bg-clip-text text-transparent bg-gradient-to-l from-blue-700 to-blue-500">
+      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8 bg-clip-text text-transparent bg-gradient-to-l from-blue-700 to-blue-500">
         {greeting}
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
