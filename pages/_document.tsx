ffe11f7c387e858b7ab5694c92766a575c8fa58d
wzulfikar <me@wzulfikar.com>
2021-04-10 11:45:30 +0700

    chore(analytic): add fathom

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,9 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script src="https://cdn.usefathom.com/script.js" data-site="NJFJHCZE" defer></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
